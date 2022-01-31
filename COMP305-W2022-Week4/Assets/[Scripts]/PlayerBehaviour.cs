683266f77ab88b108aa87ee50be5b4c7dc314cce
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-01-31 12:44:03 -0500

    Added Week 4 Project Files

---
+++ COMP305-W2022-Week4/Assets/[Scripts]/PlayerBehaviour.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float horizontalForce;
+    public float verticalForce;
+
+    [Header("Ground Detection")] 
+    public Transform groundCheck;
+    public float groundRadius;
+    public LayerMask groundLayerMask;
+    public bool isGrounded;
+
+    private Rigidbody2D rigidBody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        //isGrounded = Physics2D.Linecast(transform.position, groundCheck.position, groundLayerMask);
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, groundLayerMask);
+
+        if (isGrounded)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float y = Input.GetAxisRaw("Jump");
+
+            // check if the player is moving
+            if (x != 0)
+            {
+                x = Flip(x);
+
+                // can use this to shift the animation
+            }
+
+            Vector2 move = new Vector2(x * horizontalForce, y * verticalForce);
+            rigidBody2D.AddForce(move);
+        }
+    }
+
+    private float Flip(float x)
+    {
+        // uses the Ternary operator
+        x = (x > 0) ? 1 : -1;
+
+        transform.localScale = new Vector3(x, 1.0f);
+        return x;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(groundCheck.position, groundRadius);
+        //Gizmos.DrawLine(transform.position, groundCheck.position);
+    }
+}
