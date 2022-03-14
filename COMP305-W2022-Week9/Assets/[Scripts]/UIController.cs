c5ec8ec42e7ee62fd992c14e20c3f18490f7f105
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-03-14 13:19:07 -0400

    added Project files for Week 9

---
+++ COMP305-W2022-Week9/Assets/[Scripts]/UIController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class UIController : MonoBehaviour
+{
+    public CanvasRenderer startButtonRenderer;
+    public GameObject optionsPanel;
+
+    public AudioSource selectSound;
+    public AudioSource closeSound;
+
+    public void OnStartButton_Pressed()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+
+    public void OnCloseButton_Pressed()
+    {
+        optionsPanel.SetActive(false);
+        closeSound.Play();
+    }
+
+    public void OnOptionsButton_Pressed()
+    {
+        optionsPanel.SetActive(true);
+        selectSound.Play();
+    }
+
+    public void OnStartButton_Over()
+    {
+        startButtonRenderer.SetAlpha(0.7f);
+    }
+
+    public void OnStartButton_Out()
+    {
+        startButtonRenderer.SetAlpha(1.0f);
+    }
+}
